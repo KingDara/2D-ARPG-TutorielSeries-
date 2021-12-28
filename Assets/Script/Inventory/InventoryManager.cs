bcf85369c96729a7f1ef88da960995524b72cfc6
KingDara <kingdara.studio@gmail.com>
2021-12-28 17:00:03 +0100

    Inventory_UI/InventoryPickUp

---
+++ Assets/Script/Inventory/InventoryManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InventoryManager : MonoBehaviour
+{
+    public List<ItemSo> inventory;
+    public int inventoryLenght = 63;
+    public GameObject inventoryPanel, holderSlot;
+    private GameObject slot;
+    public GameObject prefabs;
+
+    public static InventoryManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I) && !inventoryPanel.activeInHierarchy)
+        {
+            inventoryPanel.SetActive(true);
+            if (holderSlot.transform.childCount > 0)
+            {
+                foreach (Transform item in holderSlot.transform)
+                {
+                    Destroy(item.gameObject);
+                }
+            }
+
+            for (int i = 0; i < inventory.Count; i++)
+            {
+                slot = Instantiate(prefabs, transform.position, transform.rotation);
+                slot.transform.SetParent(holderSlot.transform);
+
+                if (inventory[i] != null)
+                {
+                    TextMeshProUGUI amount = slot.transform.Find("amount").GetComponent<TextMeshProUGUI>();
+                    Image img = slot.transform.Find("icon").GetComponent<Image>();
+
+                    amount.text = inventory[i].amount.ToString();
+                    img.sprite = inventory[i].icon;
+                }
+            }
+
+        }
+        else if(Input.GetKeyDown(KeyCode.I) && inventoryPanel.activeInHierarchy)
+        {
+            inventoryPanel.SetActive(false);
+        }
+
+    }
+}
---
+++ Assets/Script/ScriptableObject/ItemSo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "item", menuName = "ScriptableObject/Item", order = 1)]
+public class ItemSo : ScriptableObject
+{
+    public string title;
+    public string description;
+    public Sprite icon;
+    public int amount, goldToGive, amountToHeal;
+    public bool isStackable;
+
+    [System.Serializable]
+    public enum Type
+    {
+        Quest, Comsommable, Commun
+    }
+
+    public Type type;
+
+
+}
